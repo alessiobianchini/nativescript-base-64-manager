78c2d4402a69dece21ccb13bed49c920ea7c3681
Alessio Bianchini <alessio.bianchini@doit.zone>
2019-12-05 14:44:03 +0100

    Plugin seed

---
+++ src/base-64-manager.ios.ts
@@ -0,0 +1,5 @@
+import { Common } from './base-64-manager.common';
+
+export class Base64Manager extends Common {
+
+}
---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import { Common } from './base-64-manager.common';
+export declare class Base64Manager extends Common {
+  // define your typings manually
+  // or..
+  // take the ios or android .d.ts files and copy/paste them here
+}
